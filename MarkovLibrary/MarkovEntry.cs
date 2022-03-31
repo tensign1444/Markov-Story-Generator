153d325190dce25ea9ef5902e8ddf016469abee3
Tanner Ensign <tensign1444@gmail.com>
2022-03-31 10:04:23 -0700

    Add project files.

---
+++ MarkovLibrary/MarkovEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedListLib;
+
+namespace MarkovLibrary
+{
+    public class MarkovEntry
+    {
+        private _2022_Spring_TannerEnsign_MyListLibrary<char> suffixes;
+        private string substring;
+        private int count;
+
+        public MarkovEntry(string key)
+        {
+            this.substring = key;
+            count = 0;
+        }
+
+        public void Add(char ch)
+        {
+            count++;
+            suffixes.Add(ch);
+        }
+
+        public override string ToString()
+        {
+            return $"'{substring}' ({count}) : {suffixes}";
+        }
+    }
+}
---
+++ MarkovStories/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarkovStories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
