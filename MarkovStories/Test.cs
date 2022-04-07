bd8a250a545c210fdbbe6dfabdceae6786846e05
Tanner Ensign <tensign1444@gmail.com>
2022-04-06 22:54:08 -0700

    Small fix

--- MarkovStories/Test.cs
+++ MarkovStories/Test.cs
@@ -17,7 +17,7 @@ namespace MarkovStories
             Console.WriteLine("Please input a max story length");
             // int length = Int32.Parse(Console.ReadLine());
             int length = 4000;
-            MarkovModel model = new MarkovModel(fileName, num, length, 2);
+            MarkovModel model = new MarkovModel(fileName, num, length, 0);
 
             model.ReadFile();
 
